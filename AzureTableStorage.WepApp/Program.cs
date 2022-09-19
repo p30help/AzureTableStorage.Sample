7a4c41cd406e635c2406d1bddd17e2d16fe4668a
Mahdi Radi <mahdi.radimeybodi@rydoo.com>
2022-09-19 14:14:05 +0200

    add azurite for support locall azure storage

--- AzureTableStorage.WepApp/Program.cs
+++ AzureTableStorage.WepApp/Program.cs
@@ -7,6 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // https://www.youtube.com/watch?v=HSL1poL1VR0
 // https://microsoft.github.io/AzureTipsAndTricks/blog/tip83.html
 
+// read more for azurite
+// https://learn.microsoft.com/en-us/azure/storage/common/storage-use-azurite?tabs=docker-hub
+// https://blog.johnnyreilly.com/2021/05/15/azurite-and-table-storage-dev-container
+// https://medium.com/cloudfordummies/local-azure-storage-emulation-with-azurite-and-azure-functions-a-dummies-guide-53949f0c1f44
+// https://github.com/Azure/Azurite#introduction
+// https://hub.docker.com/_/microsoft-azure-storage-azurite
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
